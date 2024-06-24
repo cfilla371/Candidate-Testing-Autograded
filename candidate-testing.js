bc779417d3458d0d473fd0375a768a5a310bf618
cfilla371 <64501150+cfilla371@users.noreply.github.com>
2024-06-24 18:17:34 -0500

    Commit message

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,9 +12,21 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ", 
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? ",
+  ];
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3",
+];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -27,21 +39,41 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-let answer = input.question(question);
-
-candidateAnswer = answer;
-
+for(let i = 0; i < questions.length; i++) {
+  let answer = input.question(questions[i])
+  candidateAnswers.push(answer);
+};
 }
 
 function gradeQuiz(candidateAnswers) {
 
+  let score = 0;
+  let grade = 0;
+
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if(candidateAnswer === correctAnswer) {console.log("Correct!")};
-if(candidateAnswer !== correctAnswer) {console.log("Incorrect")};
+for( let i = 0; i < questions.length; i++) {
+  if(candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
+  console.log(questions[i] + ' You answered: ' 
+    + candidateAnswers[i]
+     + ' Correct');
+     score++;
+} else {
+  console.log(questions[i] + ' You answered: ' 
+    + candidateAnswers[i]
+    + ' Incorrect')
+}
+}
+
+  grade = (score/questions.length)*100;
 
 
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+console.log('Your score is ' + grade +'%');
 
+if(grade >= 80) {
+  console.log('You passed!');
+} else {
+    console.log('Sorry, you did not score high enough')
+};
 
   return grade;
 }
@@ -66,3 +98,4 @@ module.exports = {
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
 };
+
